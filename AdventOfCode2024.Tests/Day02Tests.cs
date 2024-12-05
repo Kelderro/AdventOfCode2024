7291066106ca401c7d1c56ef01b367ad051a487d
Rob op den Kelder <Rob.opdenKelder@transavia.com>
2024-12-05 08:33:57 +0100

    Completed day 02

---
+++ AdventOfCode2024.Tests/Day02Tests.cs
@@ -0,0 +1,41 @@
+using AdventOfCode2024.Day02;
+using FluentAssertions;
+
+namespace AdventOfCode2024.Tests;
+
+public class Day02Tests
+{
+    [Fact]
+    public void Part01_Sample_Test()
+    {
+        const string input = """
+                             7 6 4 2 1
+                             1 2 7 8 9
+                             9 7 6 2 1
+                             1 3 2 4 5
+                             8 6 4 4 1
+                             1 3 6 7 9
+                             """;
+        
+        var result = PuzzleDay02.Part01(input.Split(Environment.NewLine));
+
+        result.Should().Be(2);
+    }
+    
+    [Fact]
+    public void Part02_Sample_Test()
+    {
+        const string input = """
+                             7 6 4 2 1
+                             1 2 7 8 9
+                             9 7 6 2 1
+                             1 3 2 4 5
+                             8 6 4 4 1
+                             1 3 6 7 9
+                             """;
+        
+        var result = PuzzleDay02.Part02(input.Split(Environment.NewLine));
+    
+        result.Should().Be(4);
+    }
+}
--- AdventOfCode2024/Program.cs
+++ AdventOfCode2024/Program.cs
@@ -1,5 +1,10 @@
 ﻿using AdventOfCode2024.Day01;
+using AdventOfCode2024.Day02;
 
-var input = File.ReadAllLines("Day01/input.txt");
+var input = await File.ReadAllLinesAsync("Day01/input.txt").ConfigureAwait(false);
 Console.WriteLine(PuzzleDay01.Part01(input));
 Console.WriteLine(PuzzleDay01.Part02(input));
+
+input = await File.ReadAllLinesAsync("Day02/input.txt").ConfigureAwait(false);
+Console.WriteLine(PuzzleDay02.Part01(input));
+Console.WriteLine(PuzzleDay02.Part02(input));
