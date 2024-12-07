9ffdbd7b84f972e3dfbeec2a2b21efaa4d9bf047
Rob op den Kelder <Rob.opdenKelder@transavia.com>
2024-12-07 13:10:07 +0100

    Completed day 04

--- AdventOfCode2024/Program.cs
+++ AdventOfCode2024/Program.cs
@@ -1,7 +1,4 @@
-﻿using AdventOfCode2024.Day01;
-using AdventOfCode2024.Day02;
-
-var input = await File.ReadAllLinesAsync("Day01/input.txt").ConfigureAwait(false);
+﻿var input = await File.ReadAllLinesAsync("Day01/input.txt").ConfigureAwait(false);
 Console.WriteLine(AdventOfCode2024.Day01.Puzzle.Part01(input));
 Console.WriteLine(AdventOfCode2024.Day01.Puzzle.Part02(input));
 
