4c131259cdcb32a4ec0c9b7061a08b36a764ad28
Rob op den Kelder <Rob.opdenKelder@transavia.com>
2024-12-07 13:09:09 +0100

    Completed day 03

--- AdventOfCode2024/Program.cs
+++ AdventOfCode2024/Program.cs
@@ -2,13 +2,17 @@
 using AdventOfCode2024.Day02;
 
 var input = await File.ReadAllLinesAsync("Day01/input.txt").ConfigureAwait(false);
-Console.WriteLine(PuzzleDay01.Part01(input));
-Console.WriteLine(PuzzleDay01.Part02(input));
 Console.WriteLine(AdventOfCode2024.Day01.Puzzle.Part01(input));
 Console.WriteLine(AdventOfCode2024.Day01.Puzzle.Part02(input));
 
 input = await File.ReadAllLinesAsync("Day02/input.txt").ConfigureAwait(false);
-Console.WriteLine(PuzzleDay02.Part01(input));
-Console.WriteLine(PuzzleDay02.Part02(input));
 Console.WriteLine(AdventOfCode2024.Day02.Puzzle.Part01(input));
 Console.WriteLine(AdventOfCode2024.Day02.Puzzle.Part02(input));
+
+input = await File.ReadAllLinesAsync("Day03/input.txt").ConfigureAwait(false);
+Console.WriteLine(AdventOfCode2024.Day03.Puzzle.Part01(input));
+Console.WriteLine(AdventOfCode2024.Day03.Puzzle.Part02(input));
+
+input = await File.ReadAllLinesAsync("Day04/input.txt").ConfigureAwait(false);
+Console.WriteLine(AdventOfCode2024.Day04.Puzzle.Part01(input));
+Console.WriteLine(AdventOfCode2024.Day04.Puzzle.Part02(input));
